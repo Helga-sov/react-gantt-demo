f78d93cc0263a8c5540e06bb9406e2f52b638085
Helga-sov <sandu.helga@gmail.com>
2022-07-06 12:02:41 +0200

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,56 @@
+const data = {
+	data: [
+		{
+			id: 1,
+			text: "Task #1",
+			start_date: "2022-07-01",
+			duration: 1,
+			progress: 0.6,
+		},
+		{
+			id: 2,
+			text: "Task #2",
+			start_date: "2022-07-02",
+			duration: 3,
+			progress: 0.4,
+		},
+		{
+			id: 3,
+			text: "Task #3",
+			start_date: "2022-07-03",
+			duration: 5,
+			progress: 0.1,
+		},
+		{
+			id: 4,
+			text: "Task #4",
+			start_date: "2022-07-04",
+			duration: 2,
+			progress: 0.9,
+		},
+		{
+			id: 5,
+			text: "Task #5",
+			start_date: "2022-07-05",
+			duration: 3,
+			progress: 0.3,
+		},
+		{
+			id: 6,
+			text: "Task #6",
+			start_date: "2022-07-06",
+			duration: 4,
+			progress: 0.8,
+		},
+		{
+			id: 7,
+			text: "Task #7",
+			start_date: "2022-07-07",
+			duration: 1,
+			progress: 0.7,
+		},
+	],
+	//links: [{ id: 1, source: 1, target: 2, type: "0" }],
+};
+
+export default data;
